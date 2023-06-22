d89d9e54083ac44a499d2bbd101a9141710278c3
breathecode6365 <pdandlur@gitam.in>
2023-06-22 20:03:20 +0530

    Fix build erros

--- app/Aboutme/page.js
+++ app/Aboutme/page.js
@@ -17,13 +17,9 @@ const Aboutme = () => {
         <h1 className='font-bold text-[32px] mb-4 '>PS Sreejan</h1>
         <p className='ss:text-[11px]'>
 
-I am a determined Chartered Accountant aspirant residing in Kuppam, Andhra Pradesh, India. Iam actively searching for opportunities with a prestigious firm where I can contribute my skills and make a significant impact.
-
+I am a determined Chartered Accountant aspirant residing in Kuppam, Andhra Pradesh, India. I am actively searching for opportunities with a prestigious firm where I can contribute my skills and make a significant impact.
 Driven by a strong desire to excel in the corporate world, I am eager to work in reputable companies that value excellence. With an unwavering commitment to my field, I constantly seek opportunities to showcase my abilities and deliver exceptional results. Through meticulous attention to detail, accurate financial analysis, and innovative problem-solving, I aim to drive growth and profitability.
-
-I am excited to connect with prospective recruiters who recognize the value of a dedicated and ambitious Chartered Accountant aspirant. If you are seeking a passionate individual to join your team, let's initiate a conversation. Together, we can achieve outstanding success and contribute to the growth and prosperity of your organization.
-
-</p>
+I am excited to connect with prospective recruiters who recognize the value of a dedicated and ambitious Chartered Accountant aspirant. If you are seeking a passionate individual to join your team, let&apos;s initiate a conversation. Together, we can achieve outstanding success and contribute to the growth and prosperity of your organization.</p>
         </div>
         </div>
       </section>
--- components/AchievementCard.js
+++ components/AchievementCard.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const AchievementCard = (props) => {
   return (
     <div className='bg-[#1C1E22] p-5 flex flex-col border items-center border-5 border-slate-300 lg:w-[45vw] w-[90vw]'>
-        <Image src = {props.proof}  className='w-[400px] h-[300px]' />
+        <Image src = {props.proof} alt='certificates'  className='w-[400px] h-[300px]' />
         <div className='md:p-5 p-2 text-left'>
         <h1 className='text-slate-200 font-bold lg:text-xl text-l'>{props.head}</h1>
         <p class className= "nt:text-sm text-slate-300">{props.info}</p> 
